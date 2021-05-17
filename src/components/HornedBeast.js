84ed6439b10eb88836331e840f4fd5e977925b5c
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-05-17 15:13:05 +0300

    add-JSON-state

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
-class HornedBeast extends React.Component{
-    render(){
-        return(
-            <div>
+class HornedBeast extends React.Component {
+    //Create constructue with inhert with state to be as key for passing arrgument numberOfClike
+    constructor(props){
+        super(props);
+        this.state ={
+            numberOfFavorite :0,
+        }
+
+    }
+    //Create function for increment the counter each time when clik in it
+    incrementNumberOfFavorite =()=>{
+        this .setState({
+            
+            numberOfFavorite: this.state.numberOfFavorite +1,
+        })
+    }
+    render() {
+        return (
+
+
+            <div className='beast'>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.image_url} alt={this.props.title}/>
-                <p>{this.props.description} </p>
+                <img src={this.props.imgUrl} alt={this.props.title} title={this.props.title} />
             </div>
         )
     }
