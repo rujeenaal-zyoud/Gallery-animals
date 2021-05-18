b255027df86297ca32ae37a6518bb752529fff08
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-05-18 13:13:51 +0300

    modifiy

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -4,8 +4,8 @@ class HornedBeast extends React.Component{
     render(){
         return(
             <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.image_url} alt={this.props.title}/>
+                <h3>{this.props.title}</h3>
+                <img src={this.props.imgsrc} alt={this.props.title}/>
                 <p>{this.props.description} </p>
             </div>
         )
