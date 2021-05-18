0990ab2d08acf8354beea2e1d2f1b36912d62d73
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-05-18 11:54:15 +0300

    Name-of-Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
   
 import React from 'react';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import Header from './components/Header';
-import Main from './components/main';
+import Main from './components/Main';
 
 class App extends React.Component {
   render(){
